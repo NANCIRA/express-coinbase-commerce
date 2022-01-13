16bfbca291357873664e58cfb700ab7d6a0bcc51
NANCIRA <84852048+NANCIRA@users.noreply.github.com>
2022-01-13 03:07:28 -0600

    third commit " to add resources"

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ const {config}= require("dotenv");
 
 config();
 
-const COINBASE_API_KEY = ''
-const COINBASE_WEBHOOK_SECRET= ''
+const COINBASE_API_KEY = process.env.COINBASE_API_KEY;
+const COINBASE_WEBHOOK_SECRET= process.env.COINBASE_WEBHOOK_SECRET;
 const DOMAIN = 'http://localhost:4000'
 
 module.exports = {
--- src/index.js
+++ src/index.js
@@ -4,10 +4,25 @@ const {
     COINBASE_WEBHOOK_SECRET,
     DOMAIN
 } = require('./config')
+const {Client, resources} = require('coinbase-commerce-node')
 
-console.log(COINBASE_API_KEY,COINBASE_WEBHOOK_SECRET,DOMAIN)
+Client.init(COINBASE_API_KEY)
+
+const {Charge} = resources
 
 const app = express();
+
+app.get('/create-charge', (req, res) => {
+
+    const chargeData = {
+        name: 'Sound Effect',
+        description: 'An beautiful relax music sound effect',
+        local_price: {
+            amount: ''
+        }
+    }
+})
+Charge.create()
 
 app.listen(4000);
 console.log("server on port", 4000);
